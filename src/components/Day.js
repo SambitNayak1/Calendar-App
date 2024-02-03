713d8f050f720e58244eb9e2fdddcef66161ce63
SambitNayak1 <150003730+SambitNayak1@users.noreply.github.com>
2024-02-04 01:30:32 +0530

    Update Day.js with save, update and delete event feature :boom:

--- src/components/Day.js
+++ src/components/Day.js
@@ -9,14 +9,14 @@ const Tooltip = ({ day, event }) => {
       <p>Day: {day.format("dddd, MMMM DD")}</p>
       <p>Title: {event.title}</p>
       <p>Description: {event.description}</p>
-      <p>Location: {event.location}</p> {/* Display location */}
+      <p>Location: {event.location}</p>
     </div>
   );
 };
 
 export default function Day({ day, rowIdx }) {
   const [dayEvents, setDayEvents] = useState([]);
-  const [tooltipEvent, setTooltipEvent] = useState(null); // State for tooltip
+  const [tooltipEvent, setTooltipEvent] = useState(null);
   const {
     setDaySelected,
     setShowEventModal,
@@ -37,36 +37,44 @@ export default function Day({ day, rowIdx }) {
       : "";
   }
 
+  const handleDayClick = () => {
+    setDaySelected(day);
+    setShowEventModal(true);
+    setSelectedEvent(null); // Reset selected event when a day is clicked
+  };
+
+  const handleSavedEventClick = (evt) => {
+    setSelectedEvent(evt); // Set selected event when a saved event is clicked
+    setShowEventModal(true);
+  };
+
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
         {rowIdx === 0 && (
           <p className="text-sm mt-1">{day.format("ddd").toUpperCase()}</p>
         )}
-        <p className={`text-sm p-1 my-1 text-center  ${getCurrentDayClass()}`}>
+        <p
+          className={`text-sm p-1 my-1 text-center  ${getCurrentDayClass()}`}
+          onClick={handleDayClick}
+        >
           {day.format("DD")}
         </p>
       </header>
-      <div
-        className="flex-1 cursor-pointer"
-        onClick={() => {
-          setDaySelected(day);
-          setShowEventModal(true);
-        }}
-      >
+      <div className="flex-1 cursor-pointer">
         {dayEvents.map((evt, idx) => (
           <div
             key={idx}
-            onMouseOver={() => setTooltipEvent(evt)} // Set tooltip event on hover
-            onMouseOut={() => setTooltipEvent(null)} // Clear tooltip event on mouse out
+            onMouseOver={() => setTooltipEvent(evt)}
+            onMouseOut={() => setTooltipEvent(null)}
             className={`bg-${evt.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
+            onClick={() => handleSavedEventClick(evt)}
           >
             {evt.title}
           </div>
         ))}
       </div>
-      {tooltipEvent && <Tooltip day={day} event={tooltipEvent} />}{" "}
-      {/* Render tooltip */}
+      {tooltipEvent && <Tooltip day={day} event={tooltipEvent} />}
     </div>
   );
 }
